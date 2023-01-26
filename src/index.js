88483332cfad6b4b9c170e7df31d427f230b5a90
Sadid <sadid.jones@gmail.com>
2023-01-27 00:52:16 +0600

    @27/01/2023

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 // const element = React.createElement("h1", null, "Hello World");
 const index = 0;
+class Clock extends React.Component {
+  render() {
+    return (
+      <h1 className="heading" tabIndex={index}>
+        <span className="text">
+          Hello World - {this.props.children}{" "}
+          {new Date().toLocaleTimeString(this.props.local)}
+        </span>
+      </h1>
+    );
+  }
+}
 
-setInterval(() => {
-  const element = (
-    <h1 className="heading" tabIndex={index}>
-      <span className="text">
-        Hello World {new Date().toLocaleTimeString()}
-      </span>
-      <img src="" />
-    </h1>
-  );
-  ReactDOM.render(element, document.getElementById("root"));
-}, 1000);
+ReactDOM.render(
+  <Clock local="bn-BD">test</Clock>,
+  document.getElementById("root")
+);
