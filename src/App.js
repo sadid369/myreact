95e607511b1f98bcc0ac341051b381fefcc20b48
Sadid <sadid.jones@gmail.com>
2023-01-29 19:17:07 +0600

    @29/01/2023

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import ClockList from "./components/ClockList";
+// import ClockList from "./components/ClockList";
+import Form from "./components/Form";
 
 function App() {
   console.log("App component render");
-  const quantities = [1, 2, 3];
+
   return (
     <div>
-      <ClockList quantities={quantities} />
+      <Form />
     </div>
   );
 }
